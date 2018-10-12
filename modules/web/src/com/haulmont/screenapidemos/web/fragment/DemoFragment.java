032d1cfa92234c3e20f36eb9de9a1207ce564c08
Yuriy Artamonov <artamonov@haulmont.com>
2018-10-12 12:36:59 +0400

    Use internal repo

--- modules/web/src/com/haulmont/screenapidemos/web/fragment/DemoFragment.java
+++ modules/web/src/com/haulmont/screenapidemos/web/fragment/DemoFragment.java
@@ -2,8 +2,10 @@ package com.haulmont.screenapidemos.web.fragment;
 
 import com.haulmont.cuba.gui.screen.ScreenFragment;
 import com.haulmont.cuba.gui.screen.UiController;
+import com.haulmont.cuba.gui.screen.UiDescriptor;
 
 @UiController("demo-fragment")
+@UiDescriptor("demo-fragment.xml")
 public class DemoFragment extends ScreenFragment {
 
 }
