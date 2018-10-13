801ff494b059b9223efd97138473664f549303d6
Yuriy Artamonov <artamonov@haulmont.com>
2018-10-13 15:34:04 +0400

    Fix DataScreen

--- modules/web/src/com/haulmont/screenapidemos/web/datascreen/DataScreen.java
+++ modules/web/src/com/haulmont/screenapidemos/web/datascreen/DataScreen.java
@@ -4,14 +4,16 @@ import com.haulmont.cuba.gui.components.Label;
 import com.haulmont.cuba.gui.model.InstanceContainer;
 import com.haulmont.cuba.gui.screen.Screen;
 import com.haulmont.cuba.gui.screen.UiController;
+import com.haulmont.cuba.gui.screen.UiDescriptor;
 import com.haulmont.cuba.security.entity.User;
 
 import javax.inject.Inject;
 
 @UiController
+@UiDescriptor("datascreen.xml")
 public class DataScreen extends Screen {
     @Inject
-    protected InstanceContainer<User> userCt;
+    protected InstanceContainer<User> demoCt;
     @Inject
     protected Label<String> demoLabel;
 
