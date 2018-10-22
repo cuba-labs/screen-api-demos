5ebaf5174ac6fd0cd55867e04bbe93030d45320e
Konstantin Krivopustov <krivopustov@haulmont.com>
2018-10-22 17:41:21 +0400

    Screen data examples (WIP)

--- modules/web/src/com/haulmont/screenapidemos/web/datascreen/DataScreen.java
+++ modules/web/src/com/haulmont/screenapidemos/web/datascreen/DataScreen.java
@@ -1,20 +1,27 @@
 package com.haulmont.screenapidemos.web.datascreen;
 
 import com.haulmont.cuba.gui.components.Label;
+import com.haulmont.cuba.gui.components.TextInputField;
 import com.haulmont.cuba.gui.model.InstanceContainer;
 import com.haulmont.cuba.gui.screen.Screen;
+import com.haulmont.cuba.gui.screen.Subscribe;
 import com.haulmont.cuba.gui.screen.UiController;
 import com.haulmont.cuba.gui.screen.UiDescriptor;
 import com.haulmont.cuba.security.entity.User;
 
 import javax.inject.Inject;
 
-@UiController
+@UiController("demo-data")
 @UiDescriptor("datascreen.xml")
 public class DataScreen extends Screen {
+
     @Inject
     protected InstanceContainer<User> demoCt;
     @Inject
     protected Label<String> demoLabel;
-
+    
+    @Subscribe
+    protected void onBeforeShow(BeforeShowEvent beforeShowEvent) {
+        getScreenData().loadAll();
+    }
 }
